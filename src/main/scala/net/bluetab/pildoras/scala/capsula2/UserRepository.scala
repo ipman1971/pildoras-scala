c1eea835378bdf79ecdd7c07c52a2cf7de409ce4
Pepe Corredera <jose.corredera@bluetab.net>
2019-03-11 16:44:21 +0100

    [~] formateo de código

--- src/main/scala/net/bluetab/pildoras/scala/capsula2/UserRepository.scala
+++ src/main/scala/net/bluetab/pildoras/scala/capsula2/UserRepository.scala
@@ -9,7 +9,7 @@ object UserRepository {
   // inicializo el repositorio de ejemplo
   private val usuarios: Map[Int, Usuario] = {
     Map(1 -> Usuario(1, "Jesus", "Mazo", 28, Some("hombre")),
-        2 -> Usuario(2, "Pepe", "Lopez", 41, None))
+      2 -> Usuario(2, "Pepe", "Lopez", 41, None))
   }
 
   def findById(id: Int): Option[Usuario] = {
